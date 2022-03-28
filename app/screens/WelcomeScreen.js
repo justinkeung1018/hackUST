b122a977c863730de74f6c42db764bbc1eb9d709
SweetTomato111 <chloe.yiu.yee.man@gmail.com>
2022-03-28 12:43:38 +0100

    Added button

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { ImageBackground, StyleSheet, Dimensions, View } from "react-native";
+import { ImageBackground, StyleSheet, Dimensions, View, Text, TouchableOpacity} from "react-native";
+
+const window = Dimensions.get("window");
+const screen = Dimensions.get("screen");
 
 function WelcomeScreen(props) {
     return (
@@ -7,7 +10,12 @@ function WelcomeScreen(props) {
             style={styles.background}
             source={require('../assets/images/background.png')}
         > 
-            <View style={styles.container}></View>
+            <View style={styles.container}>
+                <TouchableOpacity style={styles.buttonContainer}>
+                    <Text style={styles.buttonText}>Get Started</Text>
+                </TouchableOpacity>
+            </View>
+
         </ImageBackground>
     );
 }
@@ -19,9 +27,29 @@ const styles = StyleSheet.create({
         width: windowWidth,
     },
     container: {
+        marginTop: 50,
         paddingLeft: 30,
         paddingRight: 30,
-    }
+        marginBottom: 30,
+    },
+    buttonContainer: {   
+        position: 'absolute',
+        top: 0,
+        left: 46,
+        borderRadius: 100,
+        backgroundColor: '#c4c4c4',
+        opacity: .5,
+        width: 298,
+    },
+    buttonText: {
+        padding: 19,
+        fontSize: 24,
+        fontFamily: 'SF-Pro-Display-Bold',
+        color: 'white',
+        alignSelf: 'center',
+        justifyContent: 'center',
+        opacity: 1,
+    },
 })
 
 const windowHeight = Dimensions.get('window').height;
