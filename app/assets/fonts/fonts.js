6397e052c7d57dfc3fa70ef172c03c8927b0dff2
justinkeung <92651832+justinkeung@users.noreply.github.com>
2022-03-29 16:53:12 +0100

    Uploaded icons

---
+++ app/assets/fonts/fonts.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+const fonts = StyleSheet.create({
+    text: {
+        fontFamily: 'SF-Pro-Text-Regular',
+        fontSize: 13,
+    }
+})
