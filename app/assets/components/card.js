1774720c9498cf6efb0022eb50835cb385a7b6b4
justinkeung <92651832+justinkeung@users.noreply.github.com>
2022-03-29 21:12:12 +0100

    Refactored globalStyles into globalColors and globalFonts

--- app/assets/components/card.js
+++ app/assets/components/card.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import { globalFonts, globalColors } from '../globalStyles'
+import { globalFonts } from '../globalFonts'
 
 export default function Card(props) {
     return (
---
+++ app/assets/globalFonts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { globalColors } from './globalColors';
+
+export const globalFonts = StyleSheet.create({
+    text: {
+        fontFamily: 'SF-Pro-Text-Bold',
+        fontSize: 40,
+        color: globalColors.darkBlue
+    }
+})
