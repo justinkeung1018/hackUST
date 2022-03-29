3aa036771b937defd727882eb391d0ec1465cd0f
SweetTomato111 <chloe.yiu.yee.man@gmail.com>
2022-03-29 21:00:28 +0100

    Edited

---
+++ app/screens/DetailsScreenRyze.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ImageBackground, StyleSheet, Dimensions, View, Text, TouchableOpacity} from "react-native";
+
+function DetailsScreenRyze(props) {
+    return (
+        <Text>FilterScreen</Text>
+    );
+}
+
+export default DetailsScreenRyze;
--- routes/homeStack.js
+++ routes/homeStack.js
@@ -3,17 +3,35 @@ import { createAppContainer } from 'react-navigation';
 import ResultsScreen from '../app/screens/ResultsScreen';
 import WelcomeScreen from '../app/screens/WelcomeScreen';
 import HomeScreen from '../app/screens/HomeScreen';
+import FilterScreen from '../app/screens/FilterScreen';
+import DetailsScreenRyze from '../app/screens/DetailsScreenRyze';
 
 
 const screens = {
-    ResultsScreen: {
-        screen: ResultsScreen
-    },  
-    WelcomeScreen: {
-        screen: WelcomeScreen
+    FilterScreen: {
+        screen: FilterScreen,
+        navigationOptions: {
+            header: null,
+        }
     },
+    WelcomeScreen: {
+        screen: WelcomeScreen,
+        navigationOptions: {
+            header: null,
+        }
+    },
+    ResultsScreen: {
+        screen: ResultsScreen,
+        navigationOptions: {
+            header: null,
+        }
+    },  
+    
     HomeScreen: {
-        screen: HomeScreen
+        screen: HomeScreen,
+        navigationOptions: {
+            header: null,
+        }
     },  
 }
 
