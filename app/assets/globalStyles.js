f2d29134f9fe4301a9292ff69c641456a39941a8
justinkeung <92651832+justinkeung@users.noreply.github.com>
2022-03-29 20:30:58 +0100

    Updated globalStyles.js

--- app/assets/globalStyles.js
+++ app/assets/globalStyles.js
@@ -3,16 +3,25 @@ import { StyleSheet } from 'react-native';
 
 export const globalFonts = StyleSheet.create({
     text: {
-        fontFamily: 'SF-Pro-Text-Regular',
-        fontSize: 13,
-        
+        // fontFamily: 'SF-Pro-Text-Regular',
+        fontSize: 13
     }
 })
 
 export const globalColors = StyleSheet.create({
-    white: '#fff', 
-    black: '#000',
-    darkBlue: '#4a7abc',
-    gray:  '#c4c4c4',
-    darkGray: '#767676'
+    white: {
+        color: '#fff'
+    }, 
+    black: {
+        color: '#000'
+    },
+    darkBlue: {
+        color: '#4a7abc'
+    },
+    gray:  {
+        color: '#c4c4c4'
+    },
+    darkGray: {
+        color: '#767676'
+    }
 })
