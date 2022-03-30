a6c3b2b717027765ea18b126d0b3af272c20be11
justinkeung <92651832+justinkeung@users.noreply.github.com>
2022-03-30 19:41:47 +0100

    Built ResultsScreen

--- app/assets/components/card.js
+++ app/assets/components/card.js
@@ -1,22 +1,69 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { Text, StyleSheet, View, Image, TouchableOpacity } from 'react-native';
 import { globalFonts } from '../globalFonts'
 import { globalColors } from '../globalColors'
 
 export default function Card(props) {
     return (
-        <View style={styles.card}>
-            <Text style={globalFonts.text}>
-                Hello
-            </Text>
-        </View>
+        <TouchableOpacity> 
+            <View style={styles.card}>
+                <View style={styles.flexbox}>
+                    <Image 
+                        style={styles.image}
+                        source={props.info.image}
+                    />
+                    <View style={styles.cardContent}>
+                        <Text style={styles.name}>
+                            {props.info.name}
+                        </Text>
+                        <Text style={[styles.info, globalFonts.text]}>
+                            {'$'.repeat(props.info.price)} · {props.info.location} · {props.info.type}
+                        </Text>
+                        <Text style={globalFonts.text}>
+                            ★ {props.info.rating}
+                        </Text>
+                        <Text style={{color: props.info.open ? 'green' : 'red' }}>
+                            Open
+                        </Text>
+                    </View>
+                </View>
+            </View>
+        </TouchableOpacity>
     );
 }
-
+ 
 const styles = StyleSheet.create({
     card: {
         height: 110,
         marginBottom: 20,
         backgroundColor: globalColors.white,
-    }
+        borderRadius: 10,
+    },
+    flexbox: {
+        flex: 1,
+        flexDirection: 'row',
+    },
+    image: {
+        height: 110,
+        width: 110,
+        backgroundColor: globalColors.black,
+        borderRadius: 10,
+    },
+    cardContent: {
+        padding: 15,
+    },
+    name: {
+        fontFamily: 'SF-Pro-Text-Bold',
+        fontSize: 18,
+        marginBottom: 3,
+    },
+    info: {
+        color: globalColors.gray,
+    },
+    rating: {
+        marginBottom: 2,
+    },
+    open: {
+
+    },
 })
--- app/screens/ResultsScreen.js
+++ app/screens/ResultsScreen.js
@@ -1,14 +1,38 @@
 import React, { useState } from 'react';
+import { Text, View, StyleSheet, FlatList } from 'react-native';
+import { globalFonts } from '../assets/globalFonts';
+import { globalStyles } from '../assets/globalStyles';
 import Card from '../assets/components/card';
 
 export default function ResultsScreen({ navigation }) {
     const [ venue, setVenue ] = useState([
-        { name: 'Ryze Hong Kong', price: '2', location: 'Quarry Bay', type: 'Trampoline', favorite: false },
-        { name: 'Bounce Inc', price: '2', location: 'Kowloon Bay', type: 'Trampoline', favorite: false}
+        { name: 'Ryze Hong Kong', price: '2', location: 'Quarry Bay', type: 'Trampoline', image: require('../assets/images/ryze.jpeg'), rating: '4.5', open: true, favorite: false, key: '1' },
+        { name: 'Bounce Inc', price: '2', location: 'Kowloon Bay', type: 'Trampoline', image: require('../assets/images/ryze.jpeg'), rating: '4.2', open: false, favorite: false, key: '2' },
+        { name: 'Ryze Hong Kong', price: '2', location: 'Quarry Bay', type: 'Trampoline', image: require('../assets/images/ryze.jpeg'), rating: '4.5', open: true, favorite: false, key: '3' },
+        { name: 'Ryze Hong Kong', price: '2', location: 'Quarry Bay', type: 'Trampoline', image: require('../assets/images/ryze.jpeg'), rating: '4.5', open: true, favorite: false, key: '4' },
+        { name: 'Ryze Hong Kong', price: '2', location: 'Quarry Bay', type: 'Trampoline', image: require('../assets/images/ryze.jpeg'), rating: '4.5', open: true, favorite: false, key: '5' },
+        { name: 'Ryze Hong Kong', price: '2', location: 'Quarry Bay', type: 'Trampoline', image: require('../assets/images/ryze.jpeg'), rating: '4.5', open: true, favorite: false, key: '6' },
+        { name: 'Ryze Hong Kong', price: '2', location: 'Quarry Bay', type: 'Trampoline', image: require('../assets/images/ryze.jpeg'), rating: '4.5', open: true, favorite: false, key: '7' },
+        { name: 'Ryze Hong Kong', price: '2', location: 'Quarry Bay', type: 'Trampoline', image: require('../assets/images/ryze.jpeg'), rating: '4.5', open: true, favorite: false, key: '8' },
     ]);
 
     return (
-        <Card>
-        </Card>
+        <View style={globalStyles.container}>
+            <FlatList
+                data={venue}
+                renderItem={({ item }) => (
+                    <Card info={item}>
+                    </Card>
+                )}
+            />
+        </View>
     );
 };
+
+const styles = StyleSheet.create({
+    // card: {
+    //     height: 110,
+    //     marginBottom: 20,
+    //     backgroundColor: globalColors.white
+    // }
+})
