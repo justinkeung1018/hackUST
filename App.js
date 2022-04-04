1a9916ade55537f9467ac28159b5c873d9e236b7
justinkeung <92651832+justinkeung@users.noreply.github.com>
2022-04-04 17:30:13 +0100

    Incorporated tab navigator into stack navigator

--- App.js
+++ App.js
@@ -1,32 +1,52 @@
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import AppLoading from "expo-app-loading";
 import { useFonts } from "expo-font";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ResultsScreen from "./app/screens/ResultsScreen";
-import Navigator from "./routes/homeStack";
+import FilterScreen from "./app/screens/FilterScreen";
+import HomeScreen from "./app/screens/HomeScreen";
+import DetailsScreenRyze from "./app/screens/DetailsScreenRyze";
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
 
 import Ionicons from "react-native-vector-icons/Ionicons";
 
-function HomeScreen() {
+function Home() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
 
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Settings!</Text>
-    </View>
+          if (route.name === "Home") {
+            iconName = focused
+              ? "ios-information-circle"
+              : "ios-information-circle-outline";
+          } else if (route.name === "Settings") {
+            iconName = focused ? "list-outline" : "list";
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
+      })}
+      style={styles.navBar}
+    >
+      <Tab.Screen name="Home" component={ResultsScreen} />
+      <Tab.Screen name="Settings" component={DetailsScreenRyze} />
+    </Tab.Navigator>
   );
 }
 
 const Tab = createBottomTabNavigator();
+
+const Stack = createStackNavigator();
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -46,31 +66,17 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Navigator />
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            if (route.name === "Home") {
-              iconName = focused
-                ? "ios-information-circle"
-                : "ios-information-circle-outline";
-            } else if (route.name === "Settings") {
-              iconName = focused ? "list-outline" : "list";
-            }
-
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: "tomato",
-          tabBarInactiveTintColor: "gray",
-        })}
-        style={styles.navBar}
-      >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="HomeScreen"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="ResultsScreen" component={ResultsScreen} />
+        <Stack.Screen name="FilterScreen" component={FilterScreen} />
+        <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
+        <Stack.Screen name="DetailsScreenRyze" component={DetailsScreenRyze} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
