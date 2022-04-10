bb43232f2e55f3b98b22c8f137e38c796c7962fd
SweetTomato111 <chloe.yiu.yee.man@gmail.com>
2022-04-10 18:39:38 +0100

    Profile Screen

--- app/screens/ProfileScreen.js
+++ app/screens/ProfileScreen.js
@@ -7,11 +7,15 @@ import {
 	FlatList,
 	ScrollView,
 	Image,
+    Dimensions,
 } from "react-native";
 import { globalFonts } from "../assets/globalFonts";
 import { globalColors } from "../assets/globalColors";
 import { globalStyles } from "../assets/globalStyles";
 import Ionicons from "react-native-vector-icons/Ionicons";
+
+const windowHeight = Dimensions.get("window").height;
+const windowWidth = Dimensions.get("window").width;
 
 export default class Leaderboard extends Component {
 	constructor(props) {
@@ -23,15 +27,38 @@ export default class Leaderboard extends Component {
 
 	render() {
         
+
+
 		return (
 			<View>
-                <Image source={}></Image>
-                <Text>ProfileScreen</Text>
+                <Image source={require("../assets/images/sea.jpg")} style={styles.topImage}></Image>
+                <View style={styles.background}>
+                    
+                </View>
+                
+                <Image source={require("../assets/images/avatars/ansonlo.jpg")} style={styles.profilePicture}></Image>
             </View>
 		);
 	}
 }
 
 const styles = StyleSheet.create({
-
+    background: {
+        backgroundColor: globalColors.lightBlue,
+        minHeight: "100%",
+    },
+    topImage: {
+        height: 173,
+        width: 390,
+    },
+    profilePicture :{
+        position: "absolute",
+        top: 119,
+        left: windowWidth/2-54,
+        height: 108,
+        width: 108,
+        borderRadius: 100,
+        borderColor: 'black',
+        borderWidth: 3,
+    }
 });
